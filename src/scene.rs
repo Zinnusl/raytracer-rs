9c96384f10ded010e3c7b602dbec74f59b5d1fe0
Andreas Gritzan <zinnusl@draig.de>
2023-11-28 01:20:17 +0100

    Fix absolute path

--- src/scene.rs
+++ src/scene.rs
@@ -2,5 +2,5 @@ use crate::ray;
 
 // Using Macro, modify struct to add a container for each type of hitable from folder scene/*.rs
 //
-#[add_containers_from_files("/home/zinnusl/raytracer-rs/src/scene/*.rs")]
+#[add_containers_from_files("src/scene/*.rs")]
 struct Scene {}
